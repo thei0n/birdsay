b623be07f089142728e928af878527ade4ec40b0
theion <pheion1@proton.me>
2023-12-09 19:12:57 +0400

    no argumental program

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
 import "fmt"
+import "os"
+import "io/ioutil"
+
+var print = fmt.Println
+var printf=fmt.Printf
 
 func main(){
+
+str:="CiCi"
+
+print( " .```````,")
+
+printf("' %s    ' \n", str)
+
+file, _ := os.Open("bat.txt")
+out, _ :=ioutil.ReadAll(file)
+output := string(out[:])
+fmt.Println(output)
+
 }
+
 
